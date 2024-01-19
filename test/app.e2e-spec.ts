df0304e75fcdc32f036fc477ab5ac5dd7e9c5398
steveroseik <steveroseik@aucegypt.edu>
2024-01-19 15:53:11 +0200

    second

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,7 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
+    console.log('testingg');
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
